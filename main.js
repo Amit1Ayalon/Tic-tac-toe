1382d8b7332860bdc8b9af5f701ead51ecf01b37
Amit Ayalon <ayalon1amit@gmail.com>
2021-02-22 12:00:55 +0200

    made the canvas & cells square. added turn tracker. added board array to keep track the pieces. added placesPiece() that adds to the board array. added showBoard() that display the different pieces on the board.

--- main.js
+++ main.js
@@ -1,23 +1,70 @@
-const canvasSizeX = 400;
-const canvasSizeY = 400;
+const canvasSize = 400;
+const cellsNumPerSide = 3;
+const pixelsPerCell = canvasSize / cellsNumPerSide;
+let turn = 1;
+//creating and filling the board with null objects
+let board = Array(cellsNumPerSide);
+for (let i = 0; i < board.length; i++) {
+    board[i] = Array(cellsNumPerSide).fill(0);
+}
+
 
 function setup() {
-    createCanvas(canvasSizeX, canvasSizeY);
+    createCanvas(canvasSize, canvasSize);
+    console.log(board);
 }
 
 function draw() {
-    background(255);
-    createGrid(3, 3, canvasSizeX, canvasSizeY);
+    background(50);
+    createGrid(cellsNumPerSide, canvasSize);
+    showBoard();
 }
 
-function createGrid(horizontalCellsNum, verticalCellsNum, gridSizeX, gridSizeY) {
-    const pixelsPerCellX = gridSizeX / horizontalCellsNum;
-    const pixelsPerCellY = gridSizeY / verticalCellsNum;
+function mousePressed() {
+    placePiece(mouseX, mouseY);
+}
 
-    for (let i = 0; i < verticalCellsNum; i++) {
-        for (let j = 0; j < verticalCellsNum; j++) {
+function placePiece(posX, posY) {
+    for (let i = 0; i < cellsNumPerSide; i++) {
+        if (posX > pixelsPerCell * i && posX < pixelsPerCell * (i + 1)) {
+            for (let j = 0; j < cellsNumPerSide; j++) {
+                if (posY > pixelsPerCell * j && posY < pixelsPerCell * (j + 1)) {
+                    if (board[j][i] == 0) {
+                        board[j][i] = turn;
+                        turn *= -1;
+                    } else {
+                        console.log("Place your piece in an empty position");
+                    }
+                    break;
+                }
+            }
+            break;
+        }
+    }
+}
+
+function showBoard() {
+    for (let i = 0; i < cellsNumPerSide; i++) {
+        for (let j = 0; j < cellsNumPerSide; j++) {
+            let piece = "";
+            if (board[j][i] == 1) {
+                piece = "O";
+            } else if (board[j][i] == -1) {
+                piece = "X";
+            }
+            textAlign(CENTER);
+            textSize(100);
+            text(piece, i * pixelsPerCell + pixelsPerCell / 2, j * pixelsPerCell + pixelsPerCell / 2 + textSize() / 3);
+        }
+
+    }
+}
+
+function createGrid(cellsNum) {
+    for (let i = 0; i < cellsNum; i++) {
+        for (let j = 0; j < cellsNum; j++) {
             rectMode(CORNER);
-            rect(pixelsPerCellX * j, pixelsPerCellY * i, pixelsPerCellX, pixelsPerCellY);
+            rect(pixelsPerCell * j, pixelsPerCell * i, pixelsPerCell, pixelsPerCell);
         }
     }
 }
